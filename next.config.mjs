727fef943bf98fbefee68a63dd0741ac402de4ea
Mol4un72 <nezerak72@gmail.com>
2026-06-21 18:45:52 +0300

    Initial

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
   output: "export",
   images: { unoptimized: true },
+  basePath: "/creative_attempt",
+  assetPrefix: "/creative_attempt/",
 };
 
 export default nextConfig;
